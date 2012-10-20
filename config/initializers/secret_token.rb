ad6ccb0848ce53855785aed85eee6e7bf9b073cd
garrik <federico.perotto@tiscali.it>
2012-10-20 16:57:02 +0200

    rails new miscela

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Miscela::Application.config.secret_token = '751a9fa4c999de7bf347d00788abf0b139761f81f656ebc5f24a1ab694ee19340b6708f0a0ad5e8d3b043ef3eec10f03c7fc12589fe78ed00bf5197bbc23acdd'
