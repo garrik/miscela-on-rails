cf413016e603789d2c1effd5270b36ddaa6c3fd4
Federico <federico.perotto@tiscali.it>
2013-02-17 11:38:21 -0500

    rspec test on articles is successful

--- spec/features/articles_spec.rb
+++ spec/features/articles_spec.rb
@@ -2,9 +2,11 @@ require 'spec_helper'
 
 describe "Articles" do
   describe "GET /articles" do
-    it "displays some articles" do
+    it "displays an article" do
+      article = Article.create :author => "erik", :title => "Primo post"
       visit articles_path
       page.should have_content 'News'
+      page.should have_content 'Primo post'
     end
   end
 end
