1832664d3a111dd86d5507604762b493f28cfc68
Federico <federico.perotto@tiscali.it>
2013-02-19 13:48:09 -0500

    article creation draft and test

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,4 +2,16 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+  
+  def create
+    #render :text => params.inspect
+    
+    #do it manually
+    #Article.new
+    #Article.save 
+    
+    Article.create params[:article]
+    redirect_to :back
+  end
+  
 end
--- spec/features/articles_spec.rb
+++ spec/features/articles_spec.rb
@@ -7,6 +7,24 @@ describe "Articles" do
       visit articles_path
       page.should have_content 'News'
       page.should have_content 'Primo post'
+      #assert_select "article" do
+        #assert_select "h3", "primo post"
+        #assert_select ".meta", "erik"
+        #meta = css_select(".meta")
+        #meta.include? "erik"
+      #end
+    end
+    
+    it "creates an article" do
+      visit articles_path
+      fill_in "article[title]", :with => "Secondo post"
+      fill_in "article[content]", :with => "bla bla bla"
+      click_button "Create Article"
+      
+      current_path.should == articles_path
+      page.should have_content 'Secondo post'
+      page.should have_content 'bla bla bla'
+      
     end
   end
 end
