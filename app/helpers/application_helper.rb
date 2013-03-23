d7a8c5906d46590bea45d256bf4d99c431e284e1
garrik <federico.perotto@tiscali.it>
2013-03-23 16:01:23 +0100

    default page title added

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  # Return page title on a per-page basis
+  def page_title(title)
+    base_title = "L'Associazione Miscela ti da il benvenuto sul suo sito ufficiale"
+    if title.empty?
+      title = base_title
+    end
+  end
 end
