59332e40e4a7f7aac5a0eed4fce09fc978e42421
garrik <federico.perotto@tiscali.it>
2013-04-27 17:15:29 +0200

    adding has_secure_password does the magic and tests pass

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,15 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :name, :password_digest, :surname, :userid
+  attr_accessible :email, :name, :surname, :userid, :password, :password_confirmation
+  has_secure_password
+
+  before_save do |user| 
+  	user.userid = user.userid.downcase
+  	user.email = user.email.downcase
+  end
+
+  validates(:userid, :presence => true, :uniqueness => true)
+  validates(:email, :presence => true, :uniqueness => true)
+  validates(:password, :presence => true, :length => { :minimum => 6 })
+  validates(:password_confirmation, :presence => true)
 end
+
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,41 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do 
+  	@user = User.new(:name => "John", :surname => "Doe", :email => "j.doe@example.com", :userid => "johndoe", :password => "password", :password_confirmation => "password")
+  end
+
+  subject { @user }  
+
+  it { should respond_to(:name) }
+  it { should respond_to(:surname) }
+  it { should respond_to(:email) }
+  it { should respond_to(:userid) }
+  it { should respond_to(:password_digest) }
+  it { should respond_to(:password) }
+  it { should respond_to(:password_confirmation) }
+
+  describe "when userid is already taken" do
+  	before do
+  	  user_with_same_userid = @user.dup
+  	  user_with_same_userid.save
+  	end
+  	it { should_not be_valid }
+  end
+
+  describe "when password is not present" do
+  	before { @user.password = @user.password_confirmation = " " }
+  	it { should_not be_valid }
+  end
+
+  describe "when passwords doesn't match" do
+  	before { @user.password_confirmation = "mismatch" }
+  	it { should_not be_valid }
+  end
+
+  describe "when password confirmation is nil" do
+  	before { @user.password_confirmation = nil }
+  	it { should_not be_valid }
+  end
+
 end
